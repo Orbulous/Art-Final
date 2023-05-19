9cd54d24d2769d12644844fc576cd15dd923e63c
Orbulous <118293080+Orbulous@users.noreply.github.com>
2023-05-19 10:08:55 -0700

    Parallax done!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,21 +4,74 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Animator anim;
+    public SpriteRenderer sr;
+    public Rigidbody2D rb;
     public float moveSpeed;
+    public float horizontalInput;
+    public bool isOnGround = true;
+    public float jumpForce;  
+    public Animator animator;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector2.right * moveSpeed * horizontalInput * Time.deltaTime);
+
+        animator.SetFloat("moveSpeed", Mathf.Abs(horizontalInput));
+
+        if (horizontalInput <= 0)
+        {
+            sr.flipX = true;
+
+        }
+        else if (horizontalInput >= 0)
+        {
+            sr.flipX = false;
+
+        }
+        if (Input.GetButtonDown("Jump") && isOnGround)
+        {
+            isOnGround = false;
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            animator.SetBool("isJumping", true);
+        }
+        else
+        {
+            StartCoroutine(JumpWait());
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            animator.SetTrigger("Winking");
+        }
 
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Platform"))
+        {
+            isOnGround = true;
+        }
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+    }
+    IEnumerator JumpWait()
+    {
+        yield return new WaitForSeconds(0.5f);
+        animator.SetBool("isJumping", false);
+    }
+
 }
